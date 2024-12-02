4207f079171596f1c390a04a1809a69f9e26daf0
Joel Ethan Batac <bjoelethan@gmail.com>
2024-12-02 22:28:11 +0800

    debug?

--- app.py
+++ app.py
@@ -32,6 +32,8 @@ if action == "View Games":
         if response['status'] != "success":
             raise Exception(response['message'])
 
+        st.text(response)
+
         games_df = pd.DataFrame(response['results']) if len(response['results']) == 1 else pd.DataFrame(response['results'][0]).concat(response['results'][1])
         games_df = games_df[['AppID', 'Name', 'Price', 'Release date', 'Developers', 'Publishers', 'Language 1', 'Language 2', 'Language 3', 'Genre 1', 'Genre 2', 'Genre 3', 'Windows', 'Mac', 'Linux']]
         st.dataframe(games_df)
